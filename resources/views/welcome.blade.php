401f1cde8d0ae1429954ebe40dbba9359d61b346
BalikowaUmar12 <www.balikowaumar@gmail.com>
2025-03-05 21:03:04 +0300

    database stracture

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/bootstrap.scss','resources/js/bootstrap.js'])
+</head>
+<body>
+   <!-- <h1 class="bg-danger">diadjijsi</h1>  -->
+    oiiio
+</body>
+</html>
